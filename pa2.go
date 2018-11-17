6bf998f1169ab6e2c5063c98c334791afefbada9
rosscwagner <rosscwagner@gmail.com>
2018-11-17 15:36:36 -0500

    Echos input

--- pa2.go
+++ pa2.go
@@ -22,6 +22,60 @@ and compare their performance
 
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+/*
+This function takes in a file and returns its contents as a string
+Parameters File
+Returns string
+*/
+func fileToStr(file *os.File) string {
+	scanner := bufio.NewScanner(file)
+	txt := ""
+	for scanner.Scan() {
+		txt += scanner.Text() + "\n"
+
+	}
+	return txt
+}
+
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: ./pa2 <input file>\n")
+		return
+	}
+
+	inputFile := os.Args[1]
+
+	file, err := os.Open(inputFile)
+
+	if err != nil {
+		file.Close()
+		log.Fatal(err)
+	}
+
+	txt := fileToStr(file)
+	file.Close()
+
+	// print contents
+	fmt.Printf("%s", txt)
+
+	// split by line
+	//lines := strings.Split(txt, "\n")
+
+	// remove comments
+	/*for i, line := range lines {
+		for j, char := range line {
+			if char == '#' {
+
+			}
+		}
+	}
+	*/
 
 }
